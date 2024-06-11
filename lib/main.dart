ab7b21d1ed604ed5a6d81a2895159ab02d5aca78
Kaio Delphino <github@kaiodelphino.com>
2024-06-10 22:03:38 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:mvc/game_state.dart';
+import 'package:mvc/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Game demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: HomePage(gameState: GameState()),
+    );
+  }
+}
